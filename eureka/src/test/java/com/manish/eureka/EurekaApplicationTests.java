c92ffdb8e0d6a16bb550ac30a92e1c22233ce329
Manish Kumar <k.manu00005@gmail.com>
2024-08-27 17:51:20 +0530

    Added eureka and restaurantListing directories to main repository

---
+++ eureka/src/test/java/com/manish/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
